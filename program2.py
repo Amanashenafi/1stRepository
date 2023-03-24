88cbbdf678e25015b67fca8008c6d2207e8269aa
Amanashenafi <128808470+Amanashenafi@users.noreply.github.com>
2023-03-25 00:40:34 +0300

    another minor detail

--- program2.py
+++ program2.py
@@ -2,6 +2,7 @@ num = input("What is your name: ")
 print("Wazz up ", num)
 print("come on talk to me, ", num)
 print("why am I not getting ur calls, ", num)
+num2 = "I can't take this shit no more"
 
 
 
